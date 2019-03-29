5d2146a57c84e204755da75690c91d3f2cdbbd44
Reginald Long <39034004+Reginald231@users.noreply.github.com>
2019-03-29 10:23:23 -0400

    Add files via upload
    The files "test_heap.cpp" and "test_priority_queue.cpp" both include a main function and will cause a linker error if they are run in the same project directory. The CMake files are not needed to run this source code.

---
+++ Lab12/cmake-build-debug/CMakeFiles/Lab12.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Lab12.dir/test_priority_queue.cpp.o"
+  "Lab12.pdb"
+  "Lab12"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Lab12.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
